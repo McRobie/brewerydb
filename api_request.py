342dce520a37f805e3750afee0047e306ce1cbce
McRobie <ryancmcrobie@gmail.com>
2019-02-10 13:18:12 -0600

    learning I/O

--- api_request.py
+++ api_request.py
@@ -5,10 +5,9 @@ from brewerydb import *
 BreweryDb.configure(API_KEY)
 
 # print(BreweryDb.search({'type':'beer','q':'unibroue'}))
-
 # print(BreweryDb.beers({'page':10}))
 
-path = '/mnt/c/Users/ryanc/OperationMeteor/brewlocal/brewerydb/all_beers.json'
-parsed = json.loads(path)
-
-print(json.dumps(parsed, indent=4, sort_keys=True))
+# Learning json I/O
+#path = '/mnt/c/Users/ryanc/OperationMeteor/brewlocal/brewerydb/all_beers.json'
+#parsed = json.loads(path)
+#print(json.dumps(parsed, indent=4, sort_keys=True))
